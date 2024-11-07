1da1cd1a8768e1f5f710963cb9fc6f31b3f13917
ZhangMengyao <377922802@qq.com>
2024-11-07 11:37:30 +0200

    ft_putstr done

---
+++ Level_0/ft_putstr/ft_putstr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+    size_t i;
+
+    i = 0;
+    if(!str)
+        return ;
+    //"return ;": return with no return value
+    while(str[i] != '\0')
+    //while(str[i]) equivalent to while(str[i] != '\0')
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
