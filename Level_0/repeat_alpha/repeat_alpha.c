2a752758d02576dedf16985a4c46b6ce4ed8c05f
ZhangMengyao <377922802@qq.com>
2024-11-08 10:27:06 +0200

    rev_print done

--- Level_0/repeat_alpha/repeat_alpha.c
+++ Level_0/repeat_alpha/repeat_alpha.c
@@ -12,6 +12,7 @@ int main(int argc, char **argv)
         while(argv[1][i])
         {
             if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+            //Or use argv[1][i] >= '65'
                 n = argv[1][i] - 'A' + 1;
             else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
                 n = argv[1][i] - 'a' + 1;
