8b55a0af72d861880bac217f2fde0f50099aa58b
ZhangMengyao <377922802@qq.com>
2024-11-08 10:06:41 +0200

    repeat_alpha done

--- Level_0/repeat_alpha/repeat_alpha.c
+++ Level_0/repeat_alpha/repeat_alpha.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
@@ -10,27 +11,24 @@ int main(int argc, char **argv)
     {
         while(argv[1][i])
         {
-            if (argv[1][i] >= 65 && argv[1][i] <= 90)
-            {
-                n = argv[1][i] - 'A';
-                while (n > 0)
-                {
-                    write(1, argv[1][i], 1);
-                    n++;
-                }
-                i++;
-            }
-            else if (argv[1][i] >= 97 && argv[1][i] <= 122)
-            {
-                n = argv[1][i] - 'a';
-                while (n > 0)
-                {
-                    write(1, argv[1][i], 1);
-                    n++;
-                }
-                i++;
-            }
+            if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+                n = argv[1][i] - 'A' + 1;
+            else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+                n = argv[1][i] - 'a' + 1;
+            else
+                n = 1;
+            while(n--)
+            //while(n--) is equivalent to while(n-- > 0).
+            //And for while(n--), the condition is checked first, and then n is decremented after the check.
+                write(1, &argv[1][i], 1);
+            i++;
         }
-    write(1, "\n", 1);
     }
+    write(1, "\n", 1);
 }
+
+//cat -e: 
+//Concatenate -end, cat is commonly used to display the contents of files, and e is for the -e option is a shortcut for --show-ends.
+//This option isn't formally "named" as a command by itself but is rather a flag to make end-of-line characters visible, 
+//represented by the $ symbol.
+//Thus, the full command is simply: cat -e filename.txt
